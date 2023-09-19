14c9279b3461b2a6548819c7f42923e601639d70
Alexander Frankish <alexfrankish@gmail.com>
2023-09-19 09:17:22 +0800

    restructured test book to now work with serve

--- app/routes/indexRoute.js
+++ app/routes/indexRoute.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const User = require('../models/userModel');
 const passport = require("passport");
-const story = require("../../FirstBook.json")
+const {book} = require("../../TestBook.js")
 const jsonPayload = { msg: null };
 const loginCheck = require('../middleware/loginCheck')
 
 router.get('/index', (req, res, next)=>{
-  res.send(story)
+  res.send(book)
 })
 
 
